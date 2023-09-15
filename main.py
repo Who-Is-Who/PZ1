eb82627e61d3449939585846899f6a08bac98b4b
pankov_n_o <redblack.rb4@gmail.com>
2023-09-15 20:26:43 +0300

    # PZ1
    Напишите программу на Python, которая считывает файл, содержащий список паролей, по одному в строке. Он проверяет каждый пароль на предмет соответствия определенным требованиям (например, не менее 8 символов, содержит как прописные, так и строчные буквы, а также хотя бы одну цифру и один специальный символ). Пароли, соответствующие требованиям, должны быть распечатаны программой. (import re)
    
    # Инструкция
    
    1) Скачять все в одну папку
    2) Запустить main.py

--- main.py
+++ main.py
@@ -5,33 +5,25 @@ def is_valid_password(password):
     # Длину пароля
     if len(password) < 8:
         return False
-
     # Содержание прописных
     if not re.search(r'[A-Z]', password):
         return False
-
     # Содержание строчных
     if not re.search(r'[a-z]', password):
         return False
-
     # Содержание цифр
     if not re.search(r'\d', password):
         return False
-
     # Содержание спец символов
     if not re.search(r'[!@#$%^&*]', password):
         return False
-
     return True
-
 # Чтение файла
 with open('password.txt', 'r') as file:
     passwords = file.read().splitlines()
-
-# Проверка паролдя и вывод
+# Проверка пароля
 valid_passwords = [password for password in passwords if is_valid_password(password)]
-
-# Ввод удовлетворяющих паролей
+# Вывод удовлетворяющих паролей
 print("Допустимые пароли:")
 for password in valid_passwords:
     print(password)
